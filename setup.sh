4bf6d1c67f3eac7701be98d1c0281f2d39c2b0b8
Zepu Zhang <zepu.zhang@gmail.com>
2016-03-15 01:30:55 -0700

    build scripts

--- setup.sh
+++ setup.sh
@@ -6,38 +6,49 @@
 
 BIN=/usr/local/bin
 
-echo "installing 'py3' into '$BIN'..."
-( echo '\
-#!env bash
 
-sudo docker run --rm -it zppz/py3:0.1 $@' | sudo tee "$BIN"/py3 1>/dev/null ) || exit 1
-sudo chmod +x "$BIN"/py3 || exit 1
+echo "installing 'py3' into '$BIN'..."
+{
+sudo tee "$BIN"/py3 1>/dev/null <<EOF
+#!/usr/bin/bash
+
+sudo docker run --rm -it zppz/py3:0.1 \$@
+EOF
+sudo chmod +x "$BIN"/py3
+} || exit 1
 
 
 echo "installing 'ipy3' into '$BIN'..."
-( echo '\
-#!env bash
+{
+sudo tee "$BIN"/ipy3 1>/dev/null <<EOF
+#!/usr/bin/bash
 
-sudo docker run --rm -it zppz/py3:0.1 ipython $@' | sudo tee "$BIN"/ipy3 1>/dev/null ) || exit 1
-sudo chmod +x "$BIN"/ipy3 || exit 1
+sudo docker run --rm -it zppz/py3:0.1 ipython \$@
+EOF
+sudo chmod +x "$BIN"/ipy3
+} || exit 1
 
 
 echo "installing 'py3r' into '$BIN'..."
-( echo '\
-#!env bash
+{
+sudo tee "$BIN"/py3r 1>/dev/null <<EOF
+#!/usr/bin/bash
 
-sudo docker run --rm -it zppz/py3r:0.1 $@' | sudo tee "$BIN"/py3r 1>/dev/null ) || exit 1
-sudo chmod +x "$BIN"/py3r || exit 1
+sudo docker run --rm -it zppz/py3r:0.1 \$@
+EOF
+sudo chmod +x "$BIN"/py3r
+} || exit 1
 
 
 echo "installing 'R' into '$BIN'..."
-( echo '\
-#!env bash
+{
+sudo tee "$BIN"/R 1>/dev/null <<EOF
+#!/usr/bin/bash
 
-sudo docker run --rm -it zppz/rr:0.1 R --no-save $@' | sudo tee "$BIN"/R 1>/dev/null ) || exit 1
-sudo chmod +x "$BIN"/R || exit 1
-
-true
+sudo docker run --rm -it zppz/rr:0.1 R --no-save \$@
+EOF
+sudo chmod +x "$BIN"/R
+} || exit 1
 
 )
 
