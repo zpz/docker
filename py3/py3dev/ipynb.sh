b83a11aa637abc6955d14641588801ede0eb2ada
Zepu Zhang <zepu.zhang@gmail.com>
2016-08-28 18:29:11 -0700

    minor

--- py3/py3dev/ipynb.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo '*** Launching Jupyter Notebook server.'
-echo '*** Once the server is running, access it at "http://localhost:8888".'
-echo '*** You may use "Control-C" to terminate the server.'
-
-jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --NotebookApp.notebook_dir=/home/docker-user/
