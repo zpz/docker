5ed5ed2d654ed046a3983e10782b08ad5ed27080
Zepu Zhang <zepu.zhang@gmail.com>
2016-11-24 00:16:36 -0800

    remove the "py3" layer

--- py3/build.sh
+++ py3/build.sh
@@ -4,14 +4,16 @@ set -o pipefail
 thisfile="${BASH_SOURCE[0]}"
 thisdir="$( cd "$( dirname "${thisfile}" )" && pwd )"
 
-version=$(cat "${thisdir}"/version)
 PARENT="python:3.5.2-slim"
+
+version=$(cat "${thisdir}"/version)
 NAME="$(cat "${thisdir}/name"):${version}"
+
 
 echo
 echo =====================================================
-echo Creating Dockerfile for ${NAME}
-cat > "${thisdir}/Dockerfile" <<EOF
+echo Creating Dockerfile for "'${NAME}'"
+cat > "${thisdir}"/Dockerfile <<EOF
 # Dockerfile for image '${NAME}'
 
 #===========================
@@ -22,9 +24,6 @@ cat > "${thisdir}/Dockerfile" <<EOF
 
 FROM ${PARENT}
 
-EOF
-
-cat >> "${thisdir}/Dockerfile" <<'EOF'
 USER root
 WORKDIR /
 
@@ -32,15 +31,50 @@ EOF
 
 cat "$(dirname "${thisdir}")/img_dev_base" >> "${thisdir}/Dockerfile"
 
-cat >> "${thisdir}/Dockerfile" <<EOF
+cat >> "${thisdir}/Dockerfile" <<'EOF'
 
-#-----------
-# startup
+# Documentation
+# 'graphviz' and 'make' are to be used with Sphinx.
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+        graphviz \
+        make \
+    && pip install --no-cache-dir --upgrade \
+        'Sphinx==1.4.8' \
+    && rm -rf /var/lib/apt/lists/* /tmp/* \
+    && apt-get -y autoremove \
+    && apt-get clean
+
+# IPython, Jupyter Notebook
+# notebook requires (and will install if not available) ipython, pyzmq, tornado, jinja2 and some other things.
 
 RUN pip install --no-cache-dir --upgrade \
-        'pip==9.0.1'
+        'ipython==5.1.0' \
+    && apt-get update \
+    && apt-get install -y --no-install-recommends \
+        build-essential \
+    && pip install --no-cache-dir --upgrade \
+        'notebook==4.2.3' \
+    && apt-get purge -y --auto-remove \
+        build-essential \
+    && rm -rf /var/lib/apt/lists/* /tmp/* \
+    && apt-get -y autoremove \
+    && apt-get clean
 
-CMD ["/bin/bash"]
+# By default, Jupyter Notebook uses port 8888.
+# Launch a container with Jupyter Notebook server like this:
+# $docker run --rm -it --expose=8888 -p 8888:8888 imagename jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --NotebookApp.notebook_dir=/home/docker-user
+
+# Testing, Debugging, code analysis, code formatting
+
+RUN pip install --no-cache-dir --upgrade \
+        'ipdb==0.10.1' \
+        'pudb==2016.2' \
+        'pylint==1.6.4' \
+        'pytest==3.0.4' \
+        'yapf==0.14.0'
+
+CMD ["python"]
 EOF
 
 echo
