8d2b4d1d05d8e36959096cbf4d0b749cd5771061
Zepu Zhang <zepu.zhang@gmail.com>
2017-03-17 23:29:05 -0700

    add utility script "docker-containers.sh"

---
+++ docker-containers.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Find Docker containers whose image name contains specified substring.
+
+quiet=
+pattern=
+
+while [[ $# > 0 ]]; do
+    if [[ "$1" == '-q' ]]; then
+        quiet="yes"
+    else
+        pattern="$1"
+    fi
+    shift
+done
+
+if [[ -n "${quiet}" ]]; then
+    if [[ -n "${pattern}" ]]; then
+        docker ps -a | tail -n +2 | awk '$2 ~ "'"${pattern}"'" { print $1 }'
+    else
+        docker ps -aq
+    fi
+else
+    if [[ -n "${pattern}" ]]; then
+        header="$(docker ps | head -n 1)"
+        echo "${header}"
+        docker ps -a | tail -n +2 | awk '$2 ~ "'"${pattern}"'" { print }'
+    else
+        docker ps -a
+    fi
+fi
+
+
+
