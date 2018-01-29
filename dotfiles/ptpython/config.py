ef2881ccc61dbf578d52c4298a273830286a4dbc
Zepu Zhang <zepu.zhang@gmail.com>
2018-01-29 00:09:10 -0800

    fix ptpython mouse vs copy/paste config issue

--- dotfiles/ptpython/config.py
+++ dotfiles/ptpython/config.py
@@ -3,11 +3,11 @@ Configuration example for ``ptpython``.
 
 Copy this file to ~/.ptpython/config.py
 """
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 from prompt_toolkit.filters import ViInsertMode
-from prompt_toolkit.key_binding.input_processor import KeyPress
-from prompt_toolkit.keys import Keys
-from pygments.token import Token
+#from prompt_toolkit.key_binding.input_processor import KeyPress
+#from prompt_toolkit.keys import Keys
+#from pygments.token import Token
 
 from ptpython.layout import CompletionVisualisation
 
@@ -30,32 +30,37 @@ def configure(repl):
 
     # Show the "[Meta+Enter] Execute" message when pressing [Enter] only
     # inserts a newline instead of executing the code.
-    repl.show_meta_enter_message = True
+    #repl.show_meta_enter_message = True
 
     # Show completions. (NONE, POP_UP, MULTI_COLUMN or TOOLBAR)
-    repl.completion_visualisation = CompletionVisualisation.POP_UP
+    repl.completion_visualisation = CompletionVisualisation.MULTI_COLUMN
 
     # When CompletionVisualisation.POP_UP has been chosen, use this
     # scroll_offset in the completion menu.
-    repl.completion_menu_scroll_offset = 0
+    #repl.completion_menu_scroll_offset = 0
 
     # Show line numbers (when the input contains multiple lines.)
-    repl.show_line_numbers = False
+    #repl.show_line_numbers = False
 
     # Show status bar.
-    repl.show_status_bar = True
+    #repl.show_status_bar = True
 
     # When the sidebar is visible, also show the help text.
-    repl.show_sidebar_help = True
+    #repl.show_sidebar_help = True
 
     # Highlight matching parethesis.
     repl.highlight_matching_parenthesis = True
 
     # Line wrapping. (Instead of horizontal scrolling.)
-    repl.wrap_lines = False
+    #repl.wrap_lines = False
 
     # Mouse support.
-    repl.enable_mouse_support = True
+    #
+    # Do not enable this.
+    # Enabling this will disable copying the content of the screen
+    # and scrolling the screen.
+    #
+    repl.enable_mouse_support = False
 
     # Complete while typing. (Don't require tab before the
     # completion menu is shown.)
@@ -65,7 +70,7 @@ def configure(repl):
     repl.vi_mode = True
 
     # Paste mode. (When True, don't insert whitespace after new line.)
-    repl.paste_mode = False
+    #repl.paste_mode = False
 
     # Use the classic prompt. (Display '>>>' instead of 'In [1]'.)
     repl.prompt_style = 'classic'  # 'classic' or 'ipython'
@@ -83,15 +88,17 @@ def configure(repl):
 
     # Enable auto suggestions. (Pressing right arrow will complete the input,
     # based on the history.)
-    repl.enable_auto_suggest = False
+    #
+    # I did not see the effect of this setting.
+    repl.enable_auto_suggest = True
 
     # Enable open-in-editor. Pressing C-X C-E in emacs mode or 'v' in
     # Vi navigation mode will open the input in the current editor.
-    repl.enable_open_in_editor = True
+    #repl.enable_open_in_editor = True
 
     # Enable system prompt. Pressing meta-! will display the system prompt.
     # Also enables Control-Z suspend.
-    repl.enable_system_bindings = True
+    #repl.enable_system_bindings = True
 
     # Ask for confirmation on exit.
     repl.confirm_exit = False
@@ -106,64 +113,5 @@ def configure(repl):
 
     # Enable 24bit True color. (Not all terminals support this. -- maybe check
     # $TERM before changing.)
-    repl.true_color = False
+    #repl.true_color = False
 
-    # Install custom colorscheme named 'my-colorscheme' and use it.
-    """
-    repl.install_ui_colorscheme('my-colorscheme', _custom_ui_colorscheme)
-    repl.use_ui_colorscheme('my-colorscheme')
-    """
-
-    # Add custom key binding for PDB.
-    @repl.add_key_binding(Keys.ControlB)
-    def _(event):
-        ' Pressing Control-B will insert "pdb.set_trace()" '
-        event.cli.current_buffer.insert_text('\nimport pdb; pdb.set_trace()\n')
-
-    # Typing ControlE twice should also execute the current command.
-    # (Alternative for Meta-Enter.)
-    @repl.add_key_binding(Keys.ControlE, Keys.ControlE)
-    def _(event):
-        b = event.current_buffer
-        if b.accept_action.is_returnable:
-            b.accept_action.validate_and_handle(event.cli, b)
-
-
-    # Typing 'jj' in Vi Insert mode, should send escape. (Go back to navigation
-    # mode.)
-    @repl.add_key_binding('j', 'j', filter=ViInsertMode())
-    def _(event):
-        " Map 'jj' to Escape. "
-        event.cli.input_processor.feed(KeyPress(Keys.Escape))
-
-    """
-    # Custom key binding for some simple autocorrection while typing.
-    corrections = {
-        'impotr': 'import',
-        'pritn': 'print',
-    }
-
-    @repl.add_key_binding(' ')
-    def _(event):
-        ' When a space is pressed. Check & correct word before cursor. '
-        b = event.cli.current_buffer
-        w = b.document.get_word_before_cursor()
-
-        if w is not None:
-            if w in corrections:
-                b.delete_before_cursor(count=len(w))
-                b.insert_text(corrections[w])
-
-        b.insert_text(' ')
-    """
-
-
-# Custom colorscheme for the UI. See `ptpython/layout.py` and
-# `ptpython/style.py` for all possible tokens.
-_custom_ui_colorscheme = {
-    # Blue prompt.
-    Token.Layout.Prompt:                          'bg:#eeeeff #000000 bold',
-
-    # Make the status toolbar red.
-    Token.Toolbar.Status:                         'bg:#ff0000 #000000',
-}
--- py3/install.sh
+++ py3/install.sh
@@ -1,1 +1,5 @@
-python ../pyinstall.py --cmd=py3  --dockercmd=/bin/bash --options="--rm -it"
+python ../pyinstall.py \
+    --cmd=py3  \
+    --pypath=src/github-zpz/utilities.py \
+    --dockercmd=/bin/bash \
+    --options="--rm -it"
