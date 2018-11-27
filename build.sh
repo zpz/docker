f71444463ccc876ae3942e69132051c9e9b11731
Zepu Zhang <zepu.zhang@gmail.com>
2018-11-27 01:16:44 -0800

    minor

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ thisdir="$( cd $( dirname ${thisfile} ) && pwd )"
 
 (cd "${thisdir}"/py3 && bash build.sh)
 (cd "${thisdir}"/py3dev && bash build.sh)
+(cd "${thisdir}"/py3x && bash build.sh)
 (cd "${thisdir}"/ml && bash build.sh)
 #(cd "${thisdir}"/py3r && bash build.sh)
