fd2e8f19f6509cb7e2fa268e28145206e8ab47da
Zepu Zhang <zepu.zhang@gmail.com>
2020-03-30 11:11:23 -0700

    merge docker-tools into docker

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 A Docker base image for Python code development, emphasizing data science.
 
+A Docker utility `run-docker`.
+
--- build.sh
+++ build.sh
@@ -1,59 +1,9 @@
-# Usually this script is run w/o any argument, to build everything:
-#
-#   $ bash build.sh
-#
-# After at least one successful build of everything,
-# you can specify a particular image to build, using that image's name
-# as the argument.
-#
-# Usage:
-#    $ bash build.sh [image-name]
-
-
 set -Eeuo pipefail
 
 thisfile="${BASH_SOURCE[0]}"
 thisdir="$( cd $( dirname ${thisfile} ) && pwd )"
 
-
-function get-latest-image {
-    local name="$1"
-
-    local tag
-    local tags
-    local localimg
-    local remoteimg
-
-    tag=$(docker images "${name}" --format "{{.Tag}}" | sort | tail -n 1)
-    [[ $? == 0 ]] || { echo "${tag}"; return 1; }
-    if [[ "${tag}" == '' ]]; then
-        localimg='-'
-    else
-        localimg="${name}:${tag}"
-    fi
-
-    local url=https://hub.docker.com/v2/repositories/${name}/tags
-    tags="$(curl -L -s ${url} | tr -d '{}[]"' | tr ',' '\n' | grep name)" || tags=''
-    if [[ "$tags" == "" ]]; then
-        remoteimg='-'
-    else
-        tags="$(echo $tags | sed 's/name: //g' | sed 's/results: //g')" || return 1
-        tag=$(echo "${tags}" | tr ' ' '\n' | sort -r | head -n 1) || return 1
-        remoteimg="${name}:${tag}"
-    fi
-
-    if [[ "${localimg}" == '-' ]]; then
-        echo "${remoteimg}"
-    else
-        if [[ "${remoteimg}" == '-' ]]; then
-            echo "${localimg}"
-        elif [[ "${localimg}" < "${remoteimg}" ]]; then
-            echo "${remoteimg}"
-        else
-            echo "${localimg}"
-        fi
-    fi
-}
+source "${thisdir}/mini/bin/utils.sh"
 
 
 function main {
@@ -74,20 +24,6 @@ function main {
 
 
 NAMESPACE=zppz
-MINI_IMG_NAME=${NAMESPACE}/mini
-
-
-IMG=$(get-latest-image ${MINI_IMG_NAME}) || exit 1
-if [[ "${IMG}" == '-' ]]; then
-    >&2 echo "Unable to find image '${MINI_IMG_NAME}'"
-    exit 1
-fi
-
-# Need a few functions defined in the bash utility file `build_utils.sh`
-rm -f /tmp/build_utils.sh
-docker run --rm ${IMG} cat /usr/local/bin/utils.sh > /tmp/build_utils.sh || exit 1
-source /tmp/build_utils.sh
-rm -f /tmp/build_utils.sh
 
 # The images are pushed to Dockerhub only when built at github
 # by the integrated Travis-CI in branch `master`.
@@ -100,5 +36,5 @@ else
     PUSH=yes
 fi
 
-IMAGES=( py3 )
+IMAGES=( mini py3 )
 main
