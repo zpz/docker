04be5fdf5a0c68e05b3253da0458fafc3a21e829
zpz <zepu.zhang@gmail.com>
2023-03-21 15:51:36 -0700

    move to ubuntu22.04 and python3.10

--- py3/dotfiles/ptpython/config.py
+++ py3/dotfiles/ptpython/config.py
@@ -2,9 +2,9 @@
 Configuration example for ``ptpython``.
 
 Copy this file to $XDG_CONFIG_HOME/ptpython/config.py
+On Linux, this is: ~/.config/ptpython/config.py
+On macOS, this is: ~/Library/Application Support/ptpython/config.py
 """
-from __future__ import unicode_literals
-
 from prompt_toolkit.filters import ViInsertMode
 from prompt_toolkit.key_binding.key_processor import KeyPress
 from prompt_toolkit.keys import Keys
@@ -12,7 +12,7 @@ from prompt_toolkit.styles import Style
 
 from ptpython.layout import CompletionVisualisation
 
-__all__ = ("configure",)
+__all__ = ["configure"]
 
 
 def configure(repl):
@@ -29,9 +29,10 @@ def configure(repl):
 
     # Show the "[Meta+Enter] Execute" message when pressing [Enter] only
     # inserts a newline instead of executing the code.
-    # repl.show_meta_enter_message = True
+    repl.show_meta_enter_message = True
 
     # Show completions. (NONE, POP_UP, MULTI_COLUMN or TOOLBAR)
+    # repl.completion_visualisation = CompletionVisualisation.POP_UP
     repl.completion_visualisation = CompletionVisualisation.MULTI_COLUMN
 
     # When CompletionVisualisation.POP_UP has been chosen, use this
@@ -50,14 +51,14 @@ def configure(repl):
     # Swap light/dark colors on or off
     repl.swap_light_and_dark = False
 
-    # Highlight matching parethesis.
+    # Highlight matching parentheses.
     repl.highlight_matching_parenthesis = True
 
     # Line wrapping. (Instead of horizontal scrolling.)
     # repl.wrap_lines = True
 
     # Mouse support.
-    # repl.enable_mouse_support = True
+    repl.enable_mouse_support = True
 
     # Complete while typing. (Don't require tab before the
     # completion menu is shown.)
@@ -93,11 +94,11 @@ def configure(repl):
 
     # Enable open-in-editor. Pressing C-x C-e in emacs mode or 'v' in
     # Vi navigation mode will open the input in the current editor.
-    repl.enable_open_in_editor = True
+    # repl.enable_open_in_editor = True
 
     # Enable system prompt. Pressing meta-! will display the system prompt.
     # Also enables Control-Z suspend.
-    repl.enable_system_bindings = True
+    # repl.enable_system_bindings = True
 
     # Ask for confirmation on exit.
     repl.confirm_exit = False
@@ -107,15 +108,24 @@ def configure(repl):
     repl.enable_input_validation = True
 
     # Use this colorscheme for the code.
-    # repl.use_code_colorscheme("pastie")
+    # Ptpython uses Pygments for code styling, so you can choose from Pygments'
+    # color schemes. See:
+    # https://pygments.org/docs/styles/
+    # https://pygments.org/demo/
+    # repl.use_code_colorscheme("default")
+    # A colorscheme that looks good on dark backgrounds is 'native':
     repl.use_code_colorscheme("native")
 
     # Set color depth (keep in mind that not all terminals support true color).
 
-    # repl.color_depth = 'DEPTH_1_BIT'  # Monochrome.
-    # repl.color_depth = 'DEPTH_4_BIT'  # ANSI colors only.
+    # repl.color_depth = "DEPTH_1_BIT"  # Monochrome.
+    # repl.color_depth = "DEPTH_4_BIT"  # ANSI colors only.
     repl.color_depth = "DEPTH_8_BIT"  # The default, 256 colors.
-    # repl.color_depth = 'DEPTH_24_BIT'  # True color.
+    # repl.color_depth = "DEPTH_24_BIT"  # True color.
+
+    # Min/max brightness
+    repl.min_brightness = 0.0  # Increase for dark terminal backgrounds.
+    repl.max_brightness = 1.0  # Decrease for light terminal backgrounds.
 
     # Syntax.
     repl.enable_syntax_highlighting = True
@@ -128,22 +138,22 @@ def configure(repl):
 
     # Install custom colorscheme named 'my-colorscheme' and use it.
     """
-    repl.install_ui_colorscheme('my-colorscheme', Style.from_dict(_custom_ui_colorscheme))
-    repl.use_ui_colorscheme('my-colorscheme')
+    repl.install_ui_colorscheme("my-colorscheme", Style.from_dict(_custom_ui_colorscheme))
+    repl.use_ui_colorscheme("my-colorscheme")
     """
 
     # Add custom key binding for PDB.
     """
-    @repl.add_key_binding(Keys.ControlB)
+    @repl.add_key_binding("c-b")
     def _(event):
-        ' Pressing Control-B will insert "pdb.set_trace()" '
-        event.cli.current_buffer.insert_text('\nimport pdb; pdb.set_trace()\n')
+        " Pressing Control-B will insert "pdb.set_trace()" "
+        event.cli.current_buffer.insert_text("\nimport pdb; pdb.set_trace()\n")
     """
 
     # Typing ControlE twice should also execute the current command.
     # (Alternative for Meta-Enter.)
     """
-    @repl.add_key_binding(Keys.ControlE, Keys.ControlE)
+    @repl.add_key_binding("c-e", "c-e")
     def _(event):
         event.current_buffer.validate_and_handle()
     """
@@ -151,22 +161,22 @@ def configure(repl):
     # Typing 'jj' in Vi Insert mode, should send escape. (Go back to navigation
     # mode.)
     """
-    @repl.add_key_binding('j', 'j', filter=ViInsertMode())
+    @repl.add_key_binding("j", "j", filter=ViInsertMode())
     def _(event):
         " Map 'jj' to Escape. "
-        event.cli.key_processor.feed(KeyPress(Keys.Escape))
+        event.cli.key_processor.feed(KeyPress(Keys("escape")))
     """
 
     # Custom key binding for some simple autocorrection while typing.
     """
     corrections = {
-        'impotr': 'import',
-        'pritn': 'print',
+        "impotr": "import",
+        "pritn": "print",
     }
 
-    @repl.add_key_binding(' ')
+    @repl.add_key_binding(" ")
     def _(event):
-        ' When a space is pressed. Check & correct word before cursor. '
+        " When a space is pressed. Check & correct word before cursor. "
         b = event.cli.current_buffer
         w = b.document.get_word_before_cursor()
 
@@ -175,7 +185,7 @@ def configure(repl):
                 b.delete_before_cursor(count=len(w))
                 b.insert_text(corrections[w])
 
-        b.insert_text(' ')
+        b.insert_text(" ")
     """
 
     # Add a custom title to the status bar. This is useful when ptpython is
